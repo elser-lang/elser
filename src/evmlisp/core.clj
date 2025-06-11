3b1132fab989a8421cacd5a78b9b0e1c9cb9f495
daoio <albyullov@gmail.com>
2025-06-11 14:42:13 +0300

    feat(core): update operations

--- src/evmlisp/core.clj
+++ src/evmlisp/core.clj
@@ -4,6 +4,7 @@
             [evmlisp.printer :as prnt]
             [evmlisp.reader :as reader]))
 
+;; TODO: remove
 (def core-ns
   [['+ +]
    ['- -]
@@ -25,23 +26,32 @@
 
 (def yul-ns
   [
-   ;; Arithemtic (in progress)
-   ['+ "add(%s, %s)"]
-   ['* "mul(%s, %s)"]
-   ['- "sub(%s, %s)"]
-   ['/ "div(%s, %s)"]
-   ;['count ] todo: return lenght of data.
-   ['mod "mod(%s, %s)"]
-   ['** "exp(%s, %s)"]
+   ;; Arithemtic, binary operations.
+   ['+ (fn [x y] (format "add(%s, %s)" x y))]
+   ['* (fn [x y] (format "mul(%s, %s)" x y))]
+   ['- (fn [x y] (format "sub(%s, %s)" x y))]
+   ['/ (fn [x y] (format "div(%s, %s)" x y))]
+   ['mod (fn [x y] (format "mod(%s, %s)" x y))]
+   ['exp (fn [x y] (format "exp(%s, %s)" x y))]
 
    ;; Comparison (in progress)
-   ['= "eq(%s, %s)"]
-   ['> "gt(%s, %s)"]
-   ['< "lt(%s, %s)"]
-   ['<= "iszero(gt(%s, %s))"]
-
+   ['= (fn [x y] (format "eq(%s, %s)" x y))]
+   ['> (fn [x y] (format "gt(%s, %s)" x y))]
+   ['< (fn [x y] (format "lt(%s, %s)" x y))]
+   ['<= (fn [x y] (format "iszero(gt(%s, %s))" x y))]
+   
    ;; Logical operators (in progress)
    ['& "and(%s, %s)"]
    ['| "or(%s, %s)"]
    ['~ "not(%s)"]
    ['& "and(%s, %s)"]])
+
+(def sto-ns
+  [
+   ['read! (fn [var-name & args]
+             (format "%s(%s)" var-name
+                     (if (nil? args)
+                       ""
+                       args)))]
+   ['write! (fn [slot val] (format "sstore(%s, %s)" slot val))]
+   ])
