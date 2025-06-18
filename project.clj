b64a271a760e56d2e98ffdf34facf77f1cb73922
daoio <albyullov@gmail.com>
2025-06-18 18:52:36 +0300

    feat: update name + version

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject elser "0.1.0-SNAPSHOT"
+(defproject elser "0.0.1-alpha"
   :description "Elser - explicit and structure-enforcing language for EVM."
   
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/tools.cli "1.1.230"]
                  [org.web3j/core "4.13.0"]]
+
   
   :target-path "target/%s"
 
